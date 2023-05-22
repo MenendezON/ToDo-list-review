b9f660125c8ecdc2ed2eaa8f8d3d56224010dda9
MenendezON <menendezon@gmail.com>
2023-05-22 14:52:14 +0000

    Update review branch

---
+++ src/funct.js
@@ -0,0 +1,21 @@
+const validateForm = () => {
+  const tasks = JSON.parse(localStorage.getItem('datas')) ?? [];
+  // event.preventDefault();
+  const task = document.getElementById('newTask');
+  if (task && task.value !== '') {
+    const newTask = {
+      index: tasks.length,
+      completed: false,
+      description: task && task.value,
+    };
+    task.value = '';
+
+    tasks.push(newTask);
+    localStorage.setItem('datas', JSON.stringify(tasks));
+
+    return false;
+  }
+  return true;
+};
+
+export default validateForm;
---
+++ src/index.js
@@ -0,0 +1,108 @@
+import './style.css';
+import Icon from './option.png';
+import validateForm from './funct.js';
+import removeTask from './remove.js';
+import removeAllTask from './removeAll.js';
+
+let tasks = [];
+export const main = document.querySelector('.container');
+const dataLoading = () => {
+  tasks = JSON.parse(localStorage.getItem('datas')) ?? [];
+  main.innerHTML = '';
+  /* eslint-disable */
+  document.body.appendChild(component());
+  /* eslint-enable */
+};
+
+export default dataLoading;
+const showTask = (i) => {
+  const li = document.createElement('li');
+  const inputCheckbox = document.createElement('input');
+  const paragraph = document.createElement('input');
+  inputCheckbox.setAttribute('type', 'checkbox');
+  if (tasks[i].completed === false) {
+    inputCheckbox.removeAttribute('checked');
+  } else {
+    inputCheckbox.setAttribute('checked', 'checked');
+  }
+  inputCheckbox.addEventListener('change', () => {
+    if (inputCheckbox.checked) {
+      paragraph.classList.add('extra');
+    }
+    tasks[i].completed = inputCheckbox.checked;
+    localStorage.setItem('datas', JSON.stringify(tasks));
+    dataLoading();
+  });
+  paragraph.setAttribute('type', 'text');
+  paragraph.setAttribute('id', 'taskField');
+  paragraph.classList.add('taskField');
+  paragraph.setAttribute('value', tasks[i].description);
+  paragraph.addEventListener('change', () => {
+    tasks[i].description = paragraph.value;
+    localStorage.setItem('datas', JSON.stringify(tasks));
+    dataLoading();
+  });
+  li.appendChild(inputCheckbox);
+  li.appendChild(paragraph);
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  myIcon.setAttribute('alt', ' ');
+  myIcon.classList.add('delete');
+  myIcon.addEventListener('click', () => {
+    removeTask(i);
+    dataLoading();
+  });
+  li.appendChild(myIcon);
+  return li;
+};
+const component = () => {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'To-do list';
+  main.appendChild(h1);
+
+  const form = document.createElement('form');
+  form.setAttribute('action', '#');
+  form.setAttribute('id', 'taskForm');
+  form.addEventListener('submit', () => {
+    validateForm();
+    dataLoading();
+  });
+
+  const inputText = document.createElement('input');
+  inputText.setAttribute('type', 'text');
+  inputText.setAttribute('placeholder', 'Add a new task');
+  inputText.setAttribute('id', 'newTask');
+
+  const inputSubmit = document.createElement('input');
+  inputSubmit.setAttribute('type', 'submit');
+  inputSubmit.setAttribute('value', '>');
+
+  form.appendChild(inputText);
+  form.appendChild(inputSubmit);
+
+  main.appendChild(form);
+
+  const ul = document.createElement('ul');
+  tasks.forEach((tsk, i) => {
+    if (i >= 0) {
+      ul.appendChild(showTask(i));
+    }
+  });
+
+  main.appendChild(ul);
+
+  const inputButton = document.createElement('input');
+  inputButton.setAttribute('type', 'button');
+  inputButton.setAttribute('value', 'Clear all completd');
+  inputButton.addEventListener('click', () => {
+    removeAllTask();
+    dataLoading();
+  });
+  main.appendChild(inputButton);
+
+  return main;
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  dataLoading();
+});
